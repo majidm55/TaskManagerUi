e36003d203b63f6171b4ad414aadc155e273ceab
Majid <mohammadmajid55@gmail.com>
2025-07-28 15:00:25 -0400

    api fallbakc

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:5157/api";
 const api = axios.create({
   baseURL: apiUrl,
   withCredentials: false,
